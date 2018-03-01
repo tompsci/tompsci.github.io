59bb96d20e77991cde025c836e6f0294561a712d
themellowside <thomas.112@hotmail.co.uk>
2018-03-01 19:49:39 +0000

    try to get this working on the actual site

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://tompsci.github.io/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
